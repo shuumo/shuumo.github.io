687fedf8908aa73e6d4df36d2e5eef5ba13e23e0
shuumo <arnav.divesh.singh@gmail.com>
2025-02-08 12:54:15 +1100

    fixed divs?

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -4,17 +4,23 @@ type prp = {
     title: string;
     description: string;
     img: string;
+    href: string;
 }
+
 
 function ProjectCard(props : prp) {
     return (
         <div className="projectcard"> 
             <div className="innercard">
-                <h1>{props.title}</h1>
-                <h2>{props.description}</h2>
+                <h1><a href={props.href} target="_blank">{props.title}</a></h1> 
+                <div className="textdiv"> 
+                    <h2>{props.description}</h2>
+                </div>  
+                <div className="imgdiv">
+                    image
+                </div>
             </div>
         </div> 
-
     );
 }
 
